dcf7f4f6a3407cca07d6dbf171db24f1cea5e294
bamajoe411 <bama@bamajoe411.com>
2024-04-01 18:16:30 -0400

    Initial Commit

---
+++ TipTracker/TipTrackerApp.swift
@@ -0,0 +1,17 @@
+//
+//  TipTrackerApp.swift
+//  TipTracker
+//
+//  Created by Jeremy Beane on 4/1/24.
+//
+
+import SwiftUI
+
+@main
+struct TipTrackerApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
